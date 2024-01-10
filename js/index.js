1873421ef44d32017469b2c6aed0d3b2fd16f06f
Mpp216 <mariappereira96@gmail.com>
2024-01-10 11:50:13 +0100

    Added js
    date and hour changing in real time

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDate = losAngelesElement.querySelector(".date");
+  let losAngelesTime = losAngelesElement.querySelector(".time");
+  let currentTime = moment();
 
+  losAngelesDate.innerHTML = currentTime.format("MMMM Do YYYY");
+  losAngelesTime.innerHTML = moment
+    .tz("America/Los_Angeles")
+    .format(`h:mm:ss [<small>]A[</small>]`);
+
+  let barcelonaElement = document.querySelector("#barcelona");
+  let barcelonaDate = barcelonaElement.querySelector(".date");
+  let barcelonaTime = barcelonaElement.querySelector(".time");
+  let currentTimeBarcelona = moment();
+
+  barcelonaDate.innerHTML = currentTimeBarcelona.format("MMMM Do YYYY");
+  barcelonaTime.innerHTML = moment
+    .tz("Europe/Madrid")
+    .format(`h:mm:ss [<small>]A[</small>]`);
+}
+
+updateTime();
+setInterval(updateTime, 1000);
